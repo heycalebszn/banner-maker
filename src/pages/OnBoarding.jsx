6f3b63263f7ab7425547097c3efaff84e423cf25
caleb <kalejaiyecaleb@gmail.com>
2024-12-05 14:25:28 +0100

    commit

--- src/pages/OnBoarding.jsx
+++ src/pages/OnBoarding.jsx
@@ -6,15 +6,15 @@ const OnBoarding = () => {
     return(
         <div className="flex flex-col items-center justify-center text-center pt-[200px] relative">
             <div className="flex flex-col text-center z-[999px] relative">
-            <h1 className="text-[70px] text-white">Get your customized Social banner.</h1>
-            <p className="text-[50px] text-transparent text-white font-800">for Software Developers & Designers.</p>
+            <h1 className="md:text-[70px] text-white text-[40px]">Get your customized Social banner.</h1>
+            <p className="md:text-[50px] text-transparent text-white font-800 text-[35px]">for Software Developers & Designers.</p>
             </div>
 
             <Link to={"/get-started"}>
             <button className="bg-white text-purple-700 text-[20px] mt-[50px] w-[300px] p-[10px] rounded-[15px] font-semibold">get started! 👩‍🍳</button>
             </Link>
             <div>
-                <img className="rounded-[50px] mt-[100px] w-[1000px]" src={David} alt="david" />
+                <img className="rounded-[50px] mt-[100px] w-[1000px] md:px-[0px] px-[20px]" src={David} alt="david" />
                 
             </div>
             <div className="mt-[50px] flex gap-4">
